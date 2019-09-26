4d4f991d2b8a58e771a5b6734618a39afa7ed330
Shashwati Shradha <shashwatishradha5@gmail.com>
2019-09-26 08:51:25 -0600

    Create inorderTraversal.cpp

---
+++ inorderTraversal.cpp
@@ -0,0 +1,46 @@
+/*
+Inorder Traversal
+left -> root -> right
+
+In a BST, inorder traversal gives a sort array
+The prev element is always less than the curr element
+*/
+
+TreeNode{
+    int val;
+    TreeNode* left, right;
+    TreeNode(int x): val(x), left(nullptr), right(nullptr){}
+}
+
+//Recursive
+void inorderTraversalI(TreeNode* root){
+    if(root == nullptr) 
+        return;
+    inorderTraversalI(root->left);
+    cout<<root->val<<endl;
+    inorderTraversalI(root->right);
+}
+
+//Iterative
+void inorderTraversalII(TreeNode* root){
+    if(root == nullptr)
+        return;
+        
+    stack<TreeNode*> stk;
+    while(root){
+        stk.push(root);
+        root=root->left;
+    }
+    
+    while(!stk.empty()){
+        TreeNode* top = stk.top();
+        stk.empty();
+        cout<<top->val<<endl;
+        
+        TreeNode* right = top->right;
+        while(right){
+            stk.push(right);
+            right = right->left;
+        }
+    }
+}
